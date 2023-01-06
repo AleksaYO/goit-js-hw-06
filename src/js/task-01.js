5eaf36aa2e565a4b3e216983ca4b0ff7ffc0eb3a
AleksaYO <yura.aleksa.98@gmail.com>
2023-01-06 13:39:38 +0200

    firstTaskDone

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,17 +1,28 @@
-// const listEl = document.querySelector("#categories");
 const itemEl = document.querySelectorAll(".item");
-// console.log("itemEl", itemEl);
-
-const itemNameEl = document.querySelectorAll(" h2");
-// console.log("itemNameEl", itemNameEl);
 
 function numberOfCutegories() {
   return `Number of categories: ${itemEl.length}`;
 }
 console.log(numberOfCutegories());
 
-function cutegorysName() {
-  for (let i = 0; i < itemNameEl.length; i += 1) {
-    console.log(`Cutegory: ${itemNameEl[i].textContent}`);
+function cutegorysAndElements() {
+  for (let i = 0; i < itemEl.length; i += 1) {
+    const title = itemEl[i].firstElementChild.textContent;
+    console.log(`Cutegory: ${title}`);
+    const itemLength = itemEl[i].lastElementChild.children.length;
+    console.log(`Elements: ${itemLength}`);
   }
 }
+cutegorysAndElements();
+
+// function cutegorysName() {
+//   for (let i = 0; i < itemNameEl.length; i += 1) {
+//     console.log(`Cutegory: ${itemNameEl[i].textContent}`);
+//   }
+// }
+// cutegorysName();
+
+//   for (let x = 0; x < itemEl.length; x += 1) {
+//     const itemLength = itemEl[x].lastElementChild.children.length;
+//     console.log(itemLength);
+//   }
