4d1f6681610c92da6ba289fef41cc29a42e92a08
AleksaYO <yura.aleksa.98@gmail.com>
2023-01-07 18:25:15 +0200

    secondTaskDone

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+
+function createItemEl() {
+  for (let i = 0; i < ingredients.length; i += 1) {
+    const itemEl = document.createElement("li");
+    itemEl.classList = "item";
+    itemEl.textContent = ingredients[i];
+    listEl.appendChild(itemEl);
+    console.log(listEl);
+  }
+}
+createItemEl();
