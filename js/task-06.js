a1a3dd559c7416dfd2259ef7986069661edfb8d1
AleksaYO <yura.aleksa.98@gmail.com>
2023-01-13 22:30:48 +0200

    some fix

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,15 @@ const refs = {
 
 const dataValue = Number(refs.input.dataset.length);
 refs.input.style.outline = "transparent";
-refs.input.addEventListener("blur", inputColor);
+refs.input.addEventListener("input", inputColor);
 function inputColor(event) {
   if (event.currentTarget.value.length === dataValue) {
     refs.input.classList.add("valid");
+    refs.input.classList.remove("invalid");
   } else {
     refs.input.classList.add("invalid");
   }
 }
+/*
+'blur' показался не очень практичным для данного метода)
+*/
